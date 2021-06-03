800e8fdf468ea87cf19133302436f0a3bd8cc923
ravi312 <ravisinha312@gmail.com>
2021-06-03 14:57:51 +0530

    on click

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const birds = document.querySelectorAll(".app .bird");
       birds.forEach((bird, index) => {
-          bird.addEventListener("mouseover", function() {
+          bird.addEventListener("click", function() {
             console.log(index);
             sounds[index].currentTime = 0;
             sounds[index].play();
